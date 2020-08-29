81ee73e28f8b4b220ba997289c9aff6110e84605
David Reeves <djreeves2005@gmail.com>
2020-08-29 18:49:19 -0400

    4pm-7pm

---
+++ PolarHab/PolarHab/Data/RMessenger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolarHab.Data
+{
+    public static class RMessenger
+    {
+        public static event Action<double> ChangeProgressBar;
+        public static void OnChangeProgressBar(double progress) => ChangeProgressBar?.Invoke(progress);
+    }
+}
